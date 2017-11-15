c636a14df4743f3d73a368e371e55e7e977e086b
Guillermo Iglesias <guillermo.iglesias.90@gmail.com>
2017-11-15 12:50:39 -0300

    tonos terminados

--- tone.py
+++ tone.py
@@ -1,14 +1,14 @@
 import math, wave, array, sys
 
-'''def bandIndex(toneInput):
+def bandIndex(toneInput):
 	tones = [['1','2','3','A'],['4','5','6','B'],['7','8','9','C'],['*','0','#','D']]
 
 	up = -1
 	low = -1
 
-	for i in range(0,3):
-		for j in range(0,3):
-			if tones[i][j] == toneInput:
+	for i in range(0,4):
+		for j in range(0,4):
+			if str(tones[i][j]) == str(toneInput):
 				up = j
 				low = i
 				return up, low
@@ -22,21 +22,15 @@ print ("\n 1 2 3 A \n"
 " 7 8 9 C \n"
 " * 0 # D \n")
 
-toneInput = input("Select tone: ")
+toneInput = input("> Select tone: ")
 
 up, low = bandIndex(toneInput)
 
-print ("up: ", up)
-print ("low: ", low)
-	
 lower = [697, 770, 852, 941]
-upper = [1209, 1336, 1477, 1633]'''
+upper = [1209, 1336, 1477, 1633]
 
-#########################################################################
-#
-#  
-#
-#########################################################################
+print ("\nUpper Band:", upper[up])
+print ("Lower Band:", lower[low])
 
 duration = 1 # seconds
 volume = 20 # percent
@@ -46,8 +40,8 @@ numChan = 1 # of channels (1: mono, 2: stereo)
 dataSize = 2 # 2 bytes because of using signed short integers => bit depth = 16
 numSamples = sampleRate * duration
 
-freqLower = 697 # of cycles per second (Hz) (frequency of the sine waves)
-freqUpper = 1336 # of cycles per second (Hz) (frequency of the sine waves)
+freqLower = lower[low] # of cycles per second (Hz) (frequency of the sine waves)
+freqUpper = upper[up] # of cycles per second (Hz) (frequency of the sine waves)
 
 numSamplesPerCycLower = int(sampleRate / freqLower)
 numSamplesPerCycUpper = int(sampleRate / freqUpper)
@@ -58,12 +52,12 @@ for i in range(numSamples):
     sinLower = math.sin(math.pi * 2 * (i % numSamplesPerCycLower) / numSamplesPerCycLower)
     sinUpper = math.sin(math.pi * 2 * (i % numSamplesPerCycUpper) / numSamplesPerCycUpper)
     sample *= (sinLower + sinUpper)
-    #sample *= (sinLower)
-    print (int(sample))
     data.append(int(sample))
 
 
-f = wave.open('SineWave.wav', 'w')
+f = wave.open('Tone_' + str(toneInput) + '.wav', 'w')
 f.setparams((numChan, dataSize, sampleRate, numSamples, "NONE", "Uncompressed"))
 f.writeframes(data.tostring())
 f.close()
+
+print ("\nTone_" + str(toneInput) + ".wav created!\n")
