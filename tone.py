33a43e6d53607a8a962fc36b38d6581c7f0c2c83
Guillermo Iglesias <guillermo.iglesias.90@gmail.com>
2017-11-14 21:42:34 -0300

    tonos arreglado short int

--- tone.py
+++ tone.py
@@ -1,42 +1,69 @@
 import math, wave, array, sys
 
+'''def bandIndex(toneInput):
+	tones = [['1','2','3','A'],['4','5','6','B'],['7','8','9','C'],['*','0','#','D']]
+
+	up = -1
+	low = -1
+
+	for i in range(0,3):
+		for j in range(0,3):
+			if tones[i][j] == toneInput:
+				up = j
+				low = i
+				return up, low
+	
+	print ("\nERROR: Input is not valid\n")
+	sys.exit()	
+
+
 print ("\n 1 2 3 A \n" 
 " 4 5 6 B \n" 
 " 7 8 9 C \n"
 " * 0 # D \n")
 
-toneInput = raw_input("Select tone: ")
+toneInput = input("Select tone: ")
 
-tones = [['1','2','3','A'],['4','5','6','B'],['7','8','9','C'],['*','0','#','D']]
-tones2 = ['1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D']
+up, low = bandIndex(toneInput)
 
-#if not toneInput in tones:
-#	print "\nERROR: Input is not valid\n"
-#	sys.exit()
-print tones
-print tones2
-print tones2.index(toneInput)
+print ("up: ", up)
+print ("low: ", low)
 	
 lower = [697, 770, 852, 941]
-upper = [1209, 1336, 1477, 1633]
+upper = [1209, 1336, 1477, 1633]'''
 
+#########################################################################
+#
+#  
+#
+#########################################################################
 
 duration = 1 # seconds
-freq = 1209 # of cycles per second (Hz) (frequency of the sine waves)
-volume = 100 # percent
+volume = 20 # percent
 data = array.array('h') # signed short integer (-32768 to 32767) data
 sampleRate = 44100 # of samples per second (standard)
 numChan = 1 # of channels (1: mono, 2: stereo)
 dataSize = 2 # 2 bytes because of using signed short integers => bit depth = 16
-numSamplesPerCyc = int(sampleRate / freq)
 numSamples = sampleRate * duration
+
+freqLower = 697 # of cycles per second (Hz) (frequency of the sine waves)
+freqUpper = 1336 # of cycles per second (Hz) (frequency of the sine waves)
+
+numSamplesPerCycLower = int(sampleRate / freqLower)
+numSamplesPerCycUpper = int(sampleRate / freqUpper)
+
 
 for i in range(numSamples):
     sample = 32767 * float(volume) / 100
-    sample *= math.cos(math.pi * 2 * (i % numSamplesPerCyc) / numSamplesPerCyc)
+    sinLower = math.sin(math.pi * 2 * (i % numSamplesPerCycLower) / numSamplesPerCycLower)
+    sinUpper = math.sin(math.pi * 2 * (i % numSamplesPerCycUpper) / numSamplesPerCycUpper)
+    sample *= (sinLower + sinUpper)
+    #sample *= (sinLower)
+    print (int(sample))
     data.append(int(sample))
 
-f = wave.open('SineWave_' + str(freq) + 'Hz.wav', 'w')
+
+f = wave.open('SineWave.wav', 'w')
 f.setparams((numChan, dataSize, sampleRate, numSamples, "NONE", "Uncompressed"))
 f.writeframes(data.tostring())
 f.close()
